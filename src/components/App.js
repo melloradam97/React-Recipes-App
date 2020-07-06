fed1bb7f61c8c97be7657610c62fa4f09b07a462
melloradam97 <melloradam87@gmail.com>
2020-07-06 19:27:48 +0100

    Final App

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON. stringify(recipes))
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
   }, [recipes])
 
   const recipeContextValue = {
